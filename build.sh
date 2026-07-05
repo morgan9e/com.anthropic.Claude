4747f808e70492d3ad84917bf61bd9f49d4368be
Morgan <me@morgan.kr>
2026-07-05 22:16:37 +0900

    Add workflow

--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 set -e
 
+if [ ! -s /etc/machine-id ]; then
+  dbus-uuidgen --ensure=/etc/machine-id
+fi
+mkdir -p /var/lib/dbus
+ln -sf /etc/machine-id /var/lib/dbus/machine-id
+
 if command -v flatpak-builder >/dev/null 2>&1; then
   BUILDER="flatpak-builder"
 else
   BUILDER="flatpak run org.flatpak.Builder"
 fi
 
-$BUILDER --force-clean --user --install-deps-from=flathub \
+flatpak remote-add --user --if-not-exists \
+  flathub https://flathub.org/repo/flathub.flatpakrepo
+            if [ ! -s /etc/machine-id ]; then
+            dbus-uuidgen --ensure=/etc/machine-id
+          fi
+          mkdir -p /var/lib/dbus
+          ln -sf /etc/machine-id /var/lib/dbus/machine-id
+
+  
+$BUILDER --force-clean --disable-rofiles-fuse --user --install-deps-from=flathub \
   --repo=repo build-dir com.anthropic.Claude.yaml
 
 flatpak build-bundle --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
